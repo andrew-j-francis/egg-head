50e10ef33e7c7b4a6367934f34e6f38a4efec85e
Andrew Francis <ajfrancis102@gmail.com>
2022-05-18 16:15:53 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button>Click Me</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+</style>
