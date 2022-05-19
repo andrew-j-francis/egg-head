444ea9aef333f796685818984f1f9f02d0d0ab30
Andrew Francis <ajfrancis102@gmail.com>
2022-05-19 10:47:37 -0400

    Testing UI design

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,76 @@
 <script lang="ts">
-	export let name: string;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<button>Click Me</button>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
+    .list-container {
+
+    }
+
+    table {
+        border: 1px solid lightgray;
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    th {
+        text-align: left;
+        border: 1px solid lightgray;
+    }
+
+    tr {
+    }
+
+    input[type=text] {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+
+    tr:hover {
+        background: gray;
+    }
 </style>
+
+<div class="list-container">
+    <table>
+        <tr>
+            <th>#</th>
+            <th>Task Name</th>
+            <th>Status</th>
+            <th>Five Minutes?</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Test Task Number One</td>
+            <td>In Progress</td>
+            <td>N</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Test Task Number One</td>
+            <td>In Progress</td>
+            <td>N</td>
+        </tr>
+    </table>
+
+    <form>
+        <label for="task_name">Task Name</label>
+        <input type="text" id="task_name">
+
+        <label for="five">Is five minutes or less?</label>
+        <input type="checkbox" id="five">
+
+        <button>Create</button>
+    </form>
+</div>
